f57e47d50e9b5c6cc871450925ca36fad69cce14
Uyearit <uyearit23@gmail.com>
2025-08-08 14:25:20 +0100

    feat: update interfaces to support image generation state and props

---
+++ alx-project-0x13/components/layouts/Layout.tsx
@@ -0,0 +1,16 @@
+// components/layouts/Layout.tsx
+import { ReactComponentProps } from '@/interfaces';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout: React.FC<ReactComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
